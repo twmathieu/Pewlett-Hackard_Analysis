b4157d3c03672c8e36def35925050608259bd4b8
twmathieu <85597990+twmathieu@users.noreply.github.com>
2021-08-01 13:47:01 -0500

    push

--- schema.sql
+++ schema.sql
@@ -8,22 +8,22 @@ CREATE TABLE departments (
 
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    gender VARCHAR NOT NULL,
-    hire_date DATE NOT NULL,
-    PRIMARY KEY (emp_no)
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager (
+CREATE TABLE manager (
 dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -35,4 +35,22 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * From departments;
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_employees (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (dept_no)
+);
+
+SELECT * FROM departments;
